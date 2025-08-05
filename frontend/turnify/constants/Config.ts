dc609586e05495e1f1d2699be47020ed0499a6cc
VicenteMoscuzza <vicente.moscuzza@gmail.com>
2025-08-05 15:46:00 -0300

    Detalles finales

--- frontend/turnify/constants/Config.ts
+++ frontend/turnify/constants/Config.ts
@@ -2,10 +2,10 @@
 // export const API_BASE_URL = 'http://192.168.1.39:8000'; // Desarrollo local
 
 // Para dispositivo Android físico con Expo Go (DESARROLLO LOCAL VICHO)
- export const API_BASE_URL = 'http://192.168.0.155:8000';
+// export const API_BASE_URL = 'http://192.168.0.155:8000';
 
 // Para desarrollo local (cuando usas emulador)
 // export const API_BASE_URL = 'http://127.0.0.1:8000';
 
 // Para producción (Railway)
-// export const API_BASE_URL = 'https://tpfappsmoviles-production.up.railway.app'; 
+export const API_BASE_URL = 'https://tpfappsmoviles-production.up.railway.app'; 
